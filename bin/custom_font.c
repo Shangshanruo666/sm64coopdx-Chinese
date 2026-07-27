7dd91b289cf05cc6ddf7084862c96a0aa8e73bfe
Shangshanruo666 <qwer12341989@qq.com>
2026-07-27 15:39:35 +0800

    fix

--- bin/custom_font.c
+++ bin/custom_font.c
@@ -3,12 +3,12 @@
 #include "game/ingame_menu.h"
 #include "make_const_nonconst.h"
 
-ALIGNED8 const u8 texture_font_jp[] = {
-#include "textures/custom_font/custom_font_jp.rgba32.inc.c"
+ALIGNED8 const u8 texture_font_cn[] = {
+#include "textures/custom_font/custom_font_cn.rgba32.inc.c"
 };
 
-ALIGNED8 const u8 texture_font_jp_aliased[] = {
-#include "textures/custom_font/custom_font_jp_aliased.rgba32.inc.c"
+ALIGNED8 const u8 texture_font_emoji[] = {
+#include "textures/custom_font/custom_font_emoji.rgba32.inc.c"
 };
 
 ALIGNED8 const u8 texture_font_normal[] = {
